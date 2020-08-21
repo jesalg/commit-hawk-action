9abcb29b578abb3f9487af2245307e2f9d09bac4
Jesal Gadhia <jesal7@gmail.com>
2020-08-21 03:14:55 +0000

    Simplify interface

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const core = require('@actions/core');
 
+// https://github.com/lots0logs/gh-action-get-changed-files/blob/master/index.js#L36:16
+// https://docs.github.com/en/actions/reference/workflow-syntax-for-github-actions#onpushpull_requestpaths
 async function run() {
   try {
-    const watching = core.getInput('watching');
-    core.info(`Watching: ${watching}`);
+    const slack_webhook_url = core.getInput('slack_webhook_url');
+    core.info(`Ping: ${slack_webhook_url}`);
 
     core.setOutput('changes', []);
   } catch (error) {
